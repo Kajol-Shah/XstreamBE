c7884af985ff3ca7aeb03e607c4f5891aaef4657
Kajol-Shah <65277927+Kajol-Shah@users.noreply.github.com>
2024-12-21 21:50:46 -0500

    aap

--- app.js
+++ app.js
@@ -6,32 +6,32 @@ const configRoutes = require('./routes/index');
 
 
 const app = express();
-const static = express.static(__dirname + "/public");
+// const static = express.static(__dirname + "/public");
 
-const Handlebars = require('handlebars');
+// const Handlebars = require('handlebars');
 
-const handlebarsInstance = exphbs.create({
-  defaultLayout: 'main',
-  // Specify helpers which are only registered on this instance.
-  helpers: {
-    asJSON: (obj, spacing) => {
-      if (typeof spacing === 'number')
-        return new Handlebars.SafeString(JSON.stringify(obj, null, spacing));
+// const handlebarsInstance = exphbs.create({
+//   defaultLayout: 'main',
+//   // Specify helpers which are only registered on this instance.
+//   helpers: {
+//     asJSON: (obj, spacing) => {
+//       if (typeof spacing === 'number')
+//         return new Handlebars.SafeString(JSON.stringify(obj, null, spacing));
 
-      return new Handlebars.SafeString(JSON.stringify(obj));
-    }
-  },
-  partialsDir: ['views/partials/']
-});
+//       return new Handlebars.SafeString(JSON.stringify(obj));
+//     }
+//   },
+//   partialsDir: ['views/partials/']
+// });
 
 
 // view engine setup
 app.use(express.json())
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.engine("handlebars", handlebarsInstance.engine);
-app.set("view engine", "handlebars");
-app.use('/public', static);
+// app.engine("handlebars", handlebarsInstance.engine);
+// app.set("view engine", "handlebars");
+// app.use('/public', static);
 app.use("/",configRoutes);
 
 app.use((err, req, res, next) => {
