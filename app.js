63cec2f7c50a22e8ff7c9cff22d8888ca553cbea
Kajol-Shah <65277927+Kajol-Shah@users.noreply.github.com>
2024-12-21 21:39:47 -0500

    cc

--- app.js
+++ app.js
@@ -40,24 +40,7 @@ app.listen(3000,()=>
 
   console.log('Server started on port:3000')
 )
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
 
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   console.log(err);
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
-
-module.exports = app;
 
 
 
