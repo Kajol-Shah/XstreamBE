bd862464f7c5f6f3c15c1f897e5d22c68d4f5031
Kajol-Shah <65277927+Kajol-Shah@users.noreply.github.com>
2024-12-21 22:09:29 -0500

    apps

--- app.js
+++ app.js
@@ -11,15 +11,7 @@ const Handlebars = require('handlebars');
 
 const handlebarsInstance = exphbs.create({
   defaultLayout: 'main',
-  // Specify helpers which are only registered on this instance.
-  helpers: {
-    asJSON: (obj, spacing) => {
-      if (typeof spacing === 'number')
-        return new Handlebars.SafeString(JSON.stringify(obj, null, spacing));
-
-      return new Handlebars.SafeString(JSON.stringify(obj));
-    }
-  },
+  layoutsDir: __dirname + '/views/layouts/',
   partialsDir: ['views/partials/']
 });
 
