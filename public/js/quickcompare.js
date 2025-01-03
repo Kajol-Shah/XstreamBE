7d0dc3a7d8c90b947887f530f201a72f458732a4
Kajol-Shah <65277927+Kajol-Shah@users.noreply.github.com>
2025-01-03 16:26:58 -0500

    js change

--- public/js/quickcompare.js
+++ public/js/quickcompare.js
@@ -2,7 +2,7 @@ function toggleContainer() {
     var container = document.querySelector('.containers');
     var icon = document.getElementById('toggle-icon');
     if (container.style.maxHeight === "120px") {
-        container.style.maxHeight = "1000px"; // Set back to default height
+        container.style.maxHeight = "2000px"; // Set back to default height
         icon.innerHTML = " &#8593;"; // Show ">" when collapsed
     } else {
         container.style.maxHeight = "120px"; // Collapse container
